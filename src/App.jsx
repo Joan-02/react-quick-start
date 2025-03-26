2f5658daa8fa9b3726a3367d41376c570bd5621e
Joan Sanchis <jsalvadorsb@gmail.com>
2025-03-26 09:28:53 +0100

    ejercicio añadir tareas a un array y eliminarlas

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,45 @@
 import './App.css'
-import { UserProfile } from './UserProfile';
-import { ProductList } from './ProductList';
-import { MyButton } from './MyButton';
 import { useState } from 'react';
 
 function App() {
-  const [count, setCount] = useState(0);
+  
+  const [taskArray, setTaskArray] = useState([]);
+  const [textTask, setTextTask] = useState("");
+
+  const handleChange = (e) => {
+    setTextTask(e.target.value);
+    console.log(e);
+  }
+
+  console.log(taskArray);
+  const handleAddTask = () => {
+    if (!textTask) return;
+    setTaskArray([...taskArray, textTask ]);
+    setTextTask("");
+  }
+
+  const handleDeleteTask = (index) => {
+    const updatedTasks = taskArray.filter((task, i) => 
+      i !== index
+    );
+
+    setTaskArray(updatedTasks);
+  };
+
+  const tasks = taskArray.map((task, index) => {
+    return (
+      <li key={index}>{task}<button onClick={() => handleDeleteTask(index)}>Delete</button></li>
+    )
+  });
 
   return (
     <>
-      <div>
-        <h1>Welcome to my app</h1>
-        <MyButton count={count} setCount={setCount}/>
-        <MyButton count={count} setCount={setCount}/>
-      </div>
-      <UserProfile />
-      <ProductList />
+      <input type="text" value={textTask} onChange={handleChange}/>
+      <button onClick={handleAddTask}>Add task</button>
+      <p>Tareas creadas:</p>
+      <ul>
+        {tasks}
+      </ul>
     </>
   )
 }
